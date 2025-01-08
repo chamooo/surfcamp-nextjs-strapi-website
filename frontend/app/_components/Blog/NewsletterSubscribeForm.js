16c9580b8f24e385b974ab2a11b5a343560de0b1
Roman Melnychuk <roman4iek02@gmail.com>
2025-01-08 21:52:15 +0200

    feat(blog): Add NewsletterSubscribeForm frontend logic

--- frontend/app/_components/Blog/NewsletterSubscribeForm.js
+++ frontend/app/_components/Blog/NewsletterSubscribeForm.js
@@ -1,24 +1,51 @@
 "use client";
+
+import { useState } from "react";
+
 const NewsletterSubscribeForm = () => {
+    const [email, setEmail] = useState("");
+    const [hasSubscribed, setHasSubscribed] = useState(false);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (email) {
+            setHasSubscribed(true);
+            setEmail("");
+        }
+    };
+
     return (
         <section className="newsletter">
-            <div className="newsletter__info">
-                <h4>Subscribe to our newsletter</h4>
-                <p className="copy">
-                    Unlock Exclusive Insights and Stay In the Know – Subscribe
-                    to Our Newsletter Today to always stay in touch
-                </p>
-            </div>
-            <form className="newsletter__form">
-                <input
-                    type="text"
-                    className="newsletter__email input"
-                    placeholder="Enter your E-mail address"
-                />
-                <button className="newsletter__subscribe btn btn--medium btn--turquoise">
-                    Subscribe
-                </button>
-            </form>
+            {!hasSubscribed ? (
+                <>
+                    <div className="newsletter__info">
+                        <h4>Subscribe to our newsletter</h4>
+                        <p className="copy">
+                            Unlock Exclusive Insights and Stay In the Know –
+                            Subscribe to Our Newsletter Today to always stay in
+                            touch
+                        </p>
+                    </div>
+
+                    <form className="newsletter__form" onSubmit={onSubmit}>
+                        <input
+                            type="text"
+                            className="newsletter__email input"
+                            placeholder="Enter your E-mail address"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <button className="newsletter__subscribe btn btn--medium btn--turquoise">
+                            Subscribe
+                        </button>
+                    </form>
+                </>
+            ) : (
+                <h4 className="newsletter__thanks">
+                    Thank you for subscribing!
+                </h4>
+            )}
         </section>
     );
 };
